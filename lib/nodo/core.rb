161f0b784c1d04006272e4a73a40351c48794ff8
Matthias Grosser <mtgrosser@gmx.net>
2021-10-06 20:55:39 +0200

    Use at_exit

--- lib/nodo/core.rb
+++ lib/nodo/core.rb
@@ -105,16 +105,6 @@ module Nodo
           })()
         JS
       end
-
-      protected
-
-      def finalize(pid, tmpdir)
-        proc do
-          Process.kill(:SIGTERM, pid) rescue Errno::ECHILD
-          Process.wait(pid) rescue Errno::ECHILD
-          FileUtils.remove_entry(tmpdir) if File.directory?(tmpdir)
-        end
-      end
       
       private
       
@@ -162,7 +152,11 @@ module Nodo
       @@tmpdir = Pathname.new(Dir.mktmpdir('nodo'))
       env = Nodo.env.merge('NODE_PATH' => Nodo.modules_root.to_s)
       @@node_pid = Process.spawn(env, Nodo.binary, '-e', self.class.generate_core_code, '--', socket_path.to_s, err: :out)
-      ObjectSpace.define_finalizer(self, Core.send(:finalize, node_pid, tmpdir))
+      at_exit do
+        Process.kill(:SIGTERM, node_pid) rescue Errno::ECHILD
+        Process.wait(node_pid) rescue Errno::ECHILD
+        FileUtils.remove_entry(tmpdir) if File.directory?(tmpdir)
+      end
     end
     
     def wait_for_socket
