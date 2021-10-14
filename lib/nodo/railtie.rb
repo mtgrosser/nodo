186b29e5d6b732275ceaf81fc0d0dd4766647e3b
Matthias Grosser <mtgrosser@gmx.net>
2021-10-14 14:36:48 +0200

    Set NODE_ENV in Railtie

--- lib/nodo/railtie.rb
+++ lib/nodo/railtie.rb
@@ -4,5 +4,6 @@ require 'active_support'
 class Nodo::Railtie < Rails::Railtie
   initializer 'nodo' do |app|
     Nodo.modules_root = Rails.root.join('vendor', 'node_modules')
+    Nodo.env['NODE_ENV'] = Rails.env.to_s
   end
 end
--- lib/nodo/version.rb
+++ lib/nodo/version.rb
@@ -1,3 +1,3 @@
 module Nodo
-  VERSION = '1.5.5'
+  VERSION = '1.5.6'
 end
